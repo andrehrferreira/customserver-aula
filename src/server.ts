0ca724feb6e918e39a8ce6d25ce86a9ee89bb794
Andre <andre@tos.world>
2024-08-24 09:24:19 -0300

    use bytebuffer

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as WebSocket from "ws";
 import { v4 as uuidv4 } from 'uuid';
+import { ByteBuffer } from "./lib/bytebuffer";
+import { ConcurrentByteBufferPool } from "./lib/concurrentpool";
 
 const port = 3001;
 const clients = new Map<string, any>();
@@ -7,19 +9,22 @@ const server = new WebSocket.Server({ port });
 
 server.on('connection', (socket) => {
     const id = uuidv4();
-    socket.send(JSON.stringify({ type: "Session", id }));
+    sendPacket(socket, packetSession(id));
     clients.set(id, socket);
 
     console.log(`Player connected ${id}`);
 
     socket.on('message', (message) => {
-        const [sessionId, position] = message.toString('utf-8').split(",");
+        const [sessionId, position] = message.toString('utf-8').split("|");
+        const [x,y,z] = position.split(",");
 
-        console.log(sessionId);
-
-        if(sessionId)
-            broadcast({ type: "Move", id: sessionId, position: position }, id);
-        //console.log(`${id}: ${message.toString('utf-8')}`);
+        if(sessionId){
+            broadcast(packetMove(sessionId, { 
+                x: parseInt(x), 
+                y: parseInt(y), 
+                z: parseInt(z) 
+            }), id);
+        }
     });
 
     socket.on('close', () => {
@@ -27,14 +32,45 @@ server.on('connection', (socket) => {
         console.log(`Player disconnected ${id}`);
     });
 
-    broadcast({ type: "CreatePlayer", id: id }, id);
+    broadcast(packetCreatePlayer(id), id);
 });
 
-function broadcast(message: any, id: string){
+function sendPacket(socket: any, packet: ByteBuffer){
+    socket.send(packet.getBuffer());
+    ConcurrentByteBufferPool.release(packet);
+}
+
+function broadcast(message: ByteBuffer, id: string){
     clients.forEach((client, idSocket) => {
         if(idSocket !== id)
-            client.send(JSON.stringify(message));
-    })
+            client.send(message.getBuffer());
+    });
+
+    ConcurrentByteBufferPool.release(message);
+}
+
+function packetSession(id: string) : ByteBuffer {
+    let buffer = ConcurrentByteBufferPool.acquire();
+    buffer.putByte(0);
+    buffer.putString(id);
+    return buffer;
+}
+
+function packetCreatePlayer(id: string){
+    let buffer = ConcurrentByteBufferPool.acquire();
+    buffer.putByte(1);
+    buffer.putString(id);
+    return buffer;
+}
+
+function packetMove(id: string, positon: { x: number, y: number, z: number }){
+    let buffer = ConcurrentByteBufferPool.acquire();
+    buffer.putByte(2);
+    buffer.putString(id);
+    buffer.putInt32(positon.x);
+    buffer.putInt32(positon.y);
+    buffer.putInt32(positon.z);
+    return buffer;
 }
 
 console.log(`Server Listem ${port}`);
